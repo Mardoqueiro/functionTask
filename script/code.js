b08449520807dadba418c1fd6dba0df44eddd327
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-05-23 09:25:58 +0200

    complete

---
+++ script/code.js
@@ -0,0 +1,14 @@
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("Sir Lukembeladio");
+
+function multiply(num1, num2) {
+    console.log(num1 * num2);
+}
+multiply(5, 5);
+function tableOfFive(num) {
+    let result = num * 5;
+    console.log(result);
+
+}
